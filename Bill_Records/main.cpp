b63e931a4ce2238e294887415716b1b868c54f5e
ganlubin <ganlubin@mail.ustc.edu.cn>
2023-11-01 17:10:23 +0800

    update times

--- Bill_Records/main.cpp
+++ Bill_Records/main.cpp
@@ -58,10 +58,12 @@ void show(const std::string &swim) {
   vector.push_back("2023-10-24");
   vector.push_back("2023-10-25");
   vector.push_back("2023-10-26");
+  vector.push_back("2023-10-30");
+  vector.push_back("2023-10-31");
 
   std::cout << "-------------------------------------" << std::endl;
   std::cout << "Swimming times: " << vector.size() << std::endl;
-  std::cout << "the usage: " << vector.size() * 1.0f / 47 * 100 << '%' << '\n';
+  std::cout << "the usage: " << vector.size() * 1.0f / 51 * 100 << '%' << '\n';
   std::cout << "-------------------------------------" << std::endl;
 }
 
